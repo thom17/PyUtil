8ef747283bd256ff5d360d6aaa62cacb7946b991
DESKTOP-6F8AJAE\B <dio>
2026-02-20 13:58:52 +0900

    -add file-read/write

---
+++ filemanager/fileReader.py
@@ -0,0 +1,39 @@
+import chardet
+
+def read_file(file_path: str) -> str:
+    '''
+    파일을 읽어 문자열로 반환 (인코딩 인식 및 처리 포함)
+    '''
+    try:
+        with open(file_path, 'rb') as file:
+            raw_data = file.read()
+        file_encode = chardet.detect(raw_data)['encoding']
+
+        # 파일 읽기
+        with open(file_path, 'r', encoding=file_encode) as file:
+            return file.read()
+    except Exception as e:
+        print(f"Error reading file {file_path}: {e}")
+        return ""
+
+def write_file(file_path: str, context : str, encode = None) -> str:
+    '''
+    파일을 읽어 문자열로 반환 (인코딩 인식 및 처리 포함)
+    '''
+    try:
+        with open(file_path, 'rb') as file:
+            raw_data = file.read()
+        file_encode = chardet.detect(raw_data)['encoding']
+
+        with open(file_path, 'r', encoding=file_encode) as file:
+            before_text = file.read()
+
+        if encode is None:
+            encode = file_encode
+
+        with open(file_path, 'w', encoding=encode) as file:
+            file.write(context)
+        return before_text
+    except Exception as e:
+        print(f"Error write file {file_path}: {e}")
+        return ""
--- tests/filemanager/folder_manager_test.py
+++ tests/filemanager/folder_manager_test.py
@@ -1,7 +1,11 @@
+import os.path
+
 from filemanager.FolderManager import FolderManager
 from filemanager.window_file_open import get_folder_path
 
 import time
+
+from script.fixture_lib.diolib import target_path
 
 
 def test_folder_with_window_file_open():
@@ -17,4 +21,29 @@ def test_folder_with_window_file_open():
     total_size = file_size + dir_size
     print(f"총 크기: {total_size} 개수 별 시간 : {(end_time - start_time) * 1000 / file_size:.2f} 밀리초")
 
+import shutil
 
+def copy_file_with_dirs(org_path, target_path):
+    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    shutil.copy2(org_path, target_path)
+
+def test_copy_jpg_png():
+    source_path = get_folder_path(title='소스 선택')
+    folder_manager = FolderManager(data=source_path, skip_dir=['.vs', '.git', '.svn', 'x64', '.dll', '.lib', '.bin', '.obj', '.pdb', '.log', '.db'])
+    ext_map = folder_manager.get_type_map()
+
+    target_dir = get_folder_path(title='타겟 선택')
+
+
+    for path in ext_map['.jpg'] + ext_map['.png']:
+        org_path = os.path.join(source_path, path)
+        org_path = os.path.normpath(org_path)
+
+        target_path = os.path.join(target_dir, path)
+        target_path = os.path.normpath(target_path)
+
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+        shutil.copy2(org_path, target_path)
+
+
+test_copy_jpg_png()
